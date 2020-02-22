05b998d83080303f942b0172e48def899eddc24a
kobino <felipe.arg@gmail.com>
2020-02-22 12:10:36 +0100

    Hola mundo. Bootstrap + Jquery instalados por NPM Gulp y sus plugins instalados por NPM gulpfile.js funcionando (no funciona LiveREload con MAMP, pero sigo) Esqueleto HTML con CSS y JS cargados.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var stylus = require('gulp-stylus');
+var concatcss = require('gulp-concat-css');
+var livereload = require('gulp-livereload');
+
+gulp.task('stylus', function (done) {
+    return gulp.src('./app/stylus/*.styl')
+      .pipe(stylus())
+      .pipe(concatcss("./app/css/estilos.css"))
+      .pipe(gulp.dest('./'))
+      .pipe(livereload());
+      done()
+});
+
+gulp.task('html', function (done) {
+    return gulp.src('./app/*.html')
+      .pipe(livereload());
+      done()
+});
+
+gulp.task('watch', function (done) {
+	livereload.listen();
+    gulp.watch('./app/stylus/*.styl', gulp.series('stylus'));
+    gulp.watch('./app/*.html', gulp.series('html'));
+    done();
+});
+
+gulp.task('default', gulp.series('watch'));
